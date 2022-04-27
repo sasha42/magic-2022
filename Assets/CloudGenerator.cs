c5a4b6a3fc480f820c19929b20ee265fc44f206d
sasha42 <sasha42@users.noreply.github.com>
2022-04-27 16:33:51 +0200

    Move clouds left and right with controller

--- Assets/CloudGenerator.cs
+++ Assets/CloudGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CloudGenerator : MonoBehaviour
 {
@@ -10,14 +11,27 @@ public class CloudGenerator : MonoBehaviour
     // Control speed of movement
     public float speed;
 
+    // Button stuff
+    public InputAction button;
+    public InputAction encoder;
+    private float startScaleY;
+    float encoderVal;
+
     // Internal stuff
     GameObject myCloud;
     GameObject[] allClouds;
     int iterationLength;
+    float lastVal;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Enable button
+        startScaleY = transform.localScale.y;
+        button.Enable();
+        encoder.Enable();
+
+        // Generate clouds
         iterationLength = 1000;
         allClouds = new GameObject[iterationLength];
 
@@ -39,9 +53,33 @@ public class CloudGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Read rotary encoder value
+        encoderVal = encoder.ReadValue<float>();
+
+        // Vibrate if out of bounds
+        if (encoderVal > 1.5) {
+            Debug.Log("Too High");
+            encoderVal = lastVal;
+        } else if (encoderVal < -1.5) {
+            Debug.Log("Too Low");
+            encoderVal = lastVal;
+        }
+
+        // Compuate delta for movement
+        float moveVal = (encoderVal - lastVal);
+
+        lastVal = encoderVal;
+        // speed = lastVal;
+
+        // Debug.Log(speed);
+
         for (var i = 0; i < iterationLength; i++)
         {
             float x = allClouds[i].transform.position.x + (float)((Time.time * speed)/10000);
+            if (moveVal != 0) {
+                // Debug.Log("FIRST");
+                x = allClouds[i].transform.position.x + (float)(moveVal*10);
+            }
             float y = allClouds[i].transform.position.y;
             float z = allClouds[i].transform.position.z;
             allClouds[i].transform.position = new Vector3(x, y, z);
